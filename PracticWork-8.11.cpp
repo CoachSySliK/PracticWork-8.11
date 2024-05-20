125b8a5b87e76c71dfee151fac031b2407919602
Юрий Бояджиев <boyadziev@mail.ru>
2024-05-20 17:15:57 +0300

    Добавьте файлы проекта.

---
+++ PracticWork-8.11.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+    cout << "Enter two numbers:\n";
+    cin >> x >> y;
+    z = ((x + y) + abs(x - y)) / 2; 
+    //я не уверен, что функция модуль числа не использует операторы сравнения или циклы, я не знаю как это проверить.
+    cout << z << " is the biggest\n";
+    return 0;
+}
